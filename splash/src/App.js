198e0e491b187d4572152843860f03b7e8b59336
Ben Casalino <bencasalino@gmail.com>
2018-02-25 08:40:47 -0700

    clean up  and bemify class names

--- splash/src/App.js
+++ splash/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './social-cloud.png';
-import cloud from "./github.png";
-import team from "./circle.png";
+import ProjectLogo from './social-cloud.png';
+import GithubFlag from "./github.png";
+import TeamPlaceholderImage from "./circle.png";
 
 import knex from "./knex.svg";
 import js from "./javascript.svg";
@@ -12,67 +12,63 @@ import post from "./postman.svg";
 import react from "./react.svg";
 import twitter from "./twitter.svg";
 
-
-
-
-
 import './App.css';
 
 class App extends Component {
   render() {
     return <div className="App">
-        <div className="Flex--Wrapper">
+        <div className="App--Wrapper">
           <header>
-            <div className="header__bg" />
-            <div className="header__content">
-              <div className="header__wrapper">
-                <img className="logo" src={logo} alt="crap" />
+            <div className="Header--Gradient" />
+            <div className="Header--Content">
+              <div className="Header--Inline__Wrapper">
+                <img className="ProjectLogo" src={ProjectLogo} alt="crap" />
                 <h1>World Cloud Trends </h1>
               </div>
-              <img className="github" src={cloud} alt="poop" />
+              <img className="GithubFlag" src={GithubFlag} alt="poop" />
             </div>
-            <div className="about">
+            <div className="About--Section__Wrapper">
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                 do eiusmod tempor incididunt ut labore et dolore magna
                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                esse cillum dolore eu fugiat nulla pariatur.
               </p>
             </div>
-            <a href="" title="Button push orange" class="button btnPush btnOrange">
+            <a href="" title="Button" class="button btnPush btn">
               Check it out!
             </a>
           </header>
-          <section className="team__wrapper">
-            <h3> Project Team </h3> <img className="mini" src={logo} alt="poop" />
+          <section className="Team--Section__Wrapper">
+            <h3> Project Team </h3> <img className="ProjectLogo--Small" src={ProjectLogo} alt="poop" />
           </section>
-          <section className="about__wrapper">
+          <section className="Tech--Inline__Wrapper">
             <h4 className="used"> Technology Used: </h4>
-            <img className="tech" src={node} alt="blah" />
-            <img className="tech" src={react} alt="blah" />
-            <img className="tech" src={nodemon} alt="blah" />
-            <img className="tech" src={post} alt="blah" />
-            <img className="tech" src={js} alt="blah" />
-            <img className="tech" src={pg} alt="blah" />
-            <img className="tech" src={knex} alt="blah" />
-            <img className="tech" src={twitter} alt="blah" />
+            <img className="TechLogo" src={node} alt="blah" />
+            <img className="TechLogo" src={react} alt="blah" />
+            <img className="TechLogo" src={nodemon} alt="blah" />
+            <img className="TechLogo" src={post} alt="blah" />
+            <img className="TechLogo" src={js} alt="blah" />
+            <img className="TechLogo" src={pg} alt="blah" />
+            <img className="TechLogo" src={knex} alt="blah" />
+            <img className="TechLogo" src={twitter} alt="blah" />
           </section>
-          <section className="about__wrapper">
-            <h6 className="name"> Sarah </h6>
-            <img className="team" src={team} alt="fart" />
-            <h6 className="name"> Dan </h6>
-            <img className="team" src={team} alt="fart" />
-            <h6 className="name"> Bryan </h6>
-            <img className="team" src={team} alt="fart" />
-            <h6 className="name"> Ben </h6>
-            <img className="team" src={team} alt="fart" />
+          <section className="Team--Inline__Wrapper">
+            <h6 className="TeamMemeber--Text"> Sarah </h6>
+            <img className="TeamPlaceholderImage" src={TeamPlaceholderImage} alt="fart" />
+            <h6 className="TeamMemeber--Text"> Dan </h6>
+            <img className="TeamPlaceholderImage" src={TeamPlaceholderImage} alt="fart" />
+            <h6 className="TeamMemeber--Text"> Bryan </h6>
+            <img className="TeamPlaceholderImage" src={TeamPlaceholderImage} alt="fart" />
+            <h6 className="TeamMemeber--Text"> Ben </h6>
+            <img className="TeamPlaceholderImage" src={TeamPlaceholderImage} alt="fart" />
           </section>
+          {/* App--Wrapper */}
         </div>
         <footer> Copyright 2018 | ©</footer>
+        {/* App */}
       </div>;
 
   }
